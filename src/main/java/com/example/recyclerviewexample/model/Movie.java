0739116c25e1aa02595f0f1f436bba43fc024b3f
Bob McCone <brudderbob07@gmail.com>
2022-03-03 22:42:08 +1300

    Simple recylerview
    Made a simple recylerview, not connected to database yet

---
+++ src/main/java/com/example/recyclerviewexample/model/Movie.java
@@ -0,0 +1,25 @@
+package com.example.recyclerviewexample.model;
+
+public class Movie {
+    private String name;
+    private String year;
+    private String directorName;
+
+    public Movie(String name, String directorName, String year) {
+        this.name = name;
+        this.directorName = directorName;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
